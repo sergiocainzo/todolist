dc76ffd501cbe3c0f3446e3c3eacb7d8b4f42caa
Sergio Hermany Peter Cainzo <sergiocainzo@gmail.com>
2025-08-24 01:43:15 -0300

    Hotfix
    Correção para erro de CORS do Railway com o Swagger OpenAPI

--- src/main/java/br/com/dio/todo/API.java
+++ src/main/java/br/com/dio/todo/API.java
@@ -1,8 +1,11 @@
 package br.com.dio.todo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Cloud Server URL")})
 @SpringBootApplication
 public class API {
 
