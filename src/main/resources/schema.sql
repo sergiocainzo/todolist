aba64f52a336f6b085b732fb9105a08c48868d23
Sergio Hermany Peter Cainzo <sergiocainzo@gmail.com>
2025-08-24 15:05:55 -0300

    hotfix - conection

--- src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE tb_todos (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    descricao TEXT NOT NULL,
-    estado VARCHAR(50),
-    prioridade VARCHAR(50),
-    data_criacao TIMESTAMP
-);
